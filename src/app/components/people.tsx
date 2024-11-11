8bf604dfa7a60fdda2b8932a01526c557b87dfab
Kim Love <kim@kimlove.com>
2024-11-11 21:56:08 +0000

    feat: Added additional main person columns to results table

--- src/app/components/people.tsx
+++ src/app/components/people.tsx
@@ -3,17 +3,7 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 
-interface Person {
-  name: string;
-  url: string;
-}
-
-interface PeopleData {
-  count: number;
-  next: string | null;
-  previous: string | null;
-  results: Person[];
-}
+import { Person, PeopleData } from "@/types/apiData";
 
 export const People = () => {
   const [people, setPeople] = useState<PeopleData | null>(null);
@@ -74,6 +64,31 @@ export const People = () => {
     return <div>Error fetching people</div>;
   }
 
+  const getLink = (columnKey: keyof Person, person: Person): string => {
+    if (columnKey === "name") return person.url;
+    const value = person[columnKey];
+    return typeof value === "string" ? value : "#"; // Fallback to "#" for arrays or invalid values
+  };
+
+  const cellPadding = "p-2 px-4";
+
+  const columns: {
+    label: string;
+    key: keyof Person;
+    align?: string;
+    isLink?: boolean;
+  }[] = [
+    { label: "Name", key: "name", isLink: true, align: "text-left" },
+    { label: "Height", key: "height" },
+    { label: "Mass", key: "mass" },
+    { label: "Hair Color", key: "hair_color" },
+    { label: "Skin Colour", key: "skin_color" },
+    { label: "Eye Colour", key: "eye_color" },
+    { label: "Birth Year", key: "birth_year" },
+    { label: "Gender", key: "gender" },
+    { label: "Homeworld", key: "homeworld", isLink: true },
+  ];
+
   return (
     <div>
       <div className="my-4">
@@ -86,24 +101,52 @@ export const People = () => {
         />
       </div>
 
-      {status === "loading" ? <div>Loading...</div> : null}
+      {!people && status === "loading" ? <div>Loading...</div> : null}
 
-      {people ? (
+      {people && people.results.length >= 1 ? (
         <>
-          <ul>
-            {people.results.map((person) => {
-              // we want to get the resource type and id from the url which we're reuse to create the link
-              const link = person.url.split("/api")[1];
-
-              return (
-                <li key={person.name}>
-                  <Link href={link} className="hover:underline">
-                    {person.name}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+          <table
+            className={`bg-gray-900 w-full${
+              status === "loading" ? " opacity-30" : ""
+            }`}
+          >
+            <thead>
+              <tr className="bg-slate-700">
+                {columns.map((column, index) => (
+                  <th key={index} className={`${cellPadding} text-left`}>
+                    {column.label}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {people.results.map((person) => {
+                return (
+                  <tr key={person.name} className="capitalize">
+                    {columns.map((column) => (
+                      <td
+                        key={column.key}
+                        className={`${cellPadding} ${
+                          column.align ? column.align : "text-right"
+                        }`}
+                      >
+                        {column.isLink ? (
+                          <Link
+                            href={getLink(column.key, person)}
+                            className="font-bold underline hover:no-underline"
+                          >
+                            {column.key === "name" ? person.name : "View"}
+                          </Link>
+                        ) : (
+                          (person[column.key] as string)
+                        )}
+                      </td>
+                    ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
 
           <p className="my-4">
             Current page: <strong>{page}</strong>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { People } from "@/app/components/people";
 export default function Home() {
   return (
     <div className="">
-      <main className="">
+      <main className="max-w-6xl mx-auto border">
         <People />
       </main>
     </div>
